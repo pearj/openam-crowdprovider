4a2488edce189ad7342f8507b8450cd757c5ee14
japearson <japearson@agiledigital.com.au>
2012-10-30 11:13:05 +1100

    Patched version from Dave van Eijck

---
+++ crowdprovider/src/main/java/org/forgerock/openam/extensions/crowd/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the classes to provide SSO with OpenAM and Atlassian products.
+ */
+package org.forgerock.openam.extensions.crowd;
+
---
+++ crowdprovider/src/main/java/org/forgerock/openam/extensions/crowd/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the utility classes used to provide SSO with OpenAM and Atlassian products.
+ */
+package org.forgerock.openam.extensions.crowd.util;
+
